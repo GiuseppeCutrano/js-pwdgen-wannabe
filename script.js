77b245d3eaab22af7dbc337f32eb9daac07853c6
GiuseppeCutrano <giuseppecutrano.info@gmail.com>
2020-10-19 15:02:27 +0200

    fine

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Come ti chiami?");// nome
+
+var cognome = prompt("Quale è il tuo cognome?"); //cognome
+
+var colore = prompt("Quale è il tuo colore preferito?");// Colore preferito
+
+document.getElementById('user').innerHTML = nome + cognome + colore + "19"; // risultato
